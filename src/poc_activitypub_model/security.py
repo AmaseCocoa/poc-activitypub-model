e2e99271168ee79ebadeea5b1ec02b8a6e85b894
AmaseCocoa <cocoa@amase.cc>
2026-05-13 12:33:02 +0900

    feat: xsd:datetimeをparseできるように

--- src/poc_activitypub_model/security.py
+++ src/poc_activitypub_model/security.py
@@ -1,3 +1,6 @@
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
 from poc_activitypub_model.base import ActivityPubModel
 
 
@@ -11,5 +14,21 @@ class CryptographicKey(ActivityPubModel):
     @property
     def owner(self) -> str | None: return self._data.get("https://w3id.org/security/v1#owner")
 
-    @property # TODO: implement logic convert to publicKey Object
-    def public_key(self) -> bytes | str | None: return self._data.get("https://w3id.org/security/v1#publicKeyPem")
+    @property
+    def public_key(self) -> RSAPublicKey | Ed25519PublicKey | None:
+        public_key = self._kv.get("https://w3id.org/security/v1#publicKeyPem")
+        if public_key:
+            return public_key
+
+        data: bytes | str | None = self._data.get("https://w3id.org/security/v1#publicKeyPem")
+
+        if not data:
+            return None
+
+        key = load_pem_public_key(data if isinstance(data, bytes) else data.encode("utf-8"))
+        match key:
+            case RSAPublicKey() | Ed25519PublicKey():
+                self._kv["https://w3id.org/security/v1#publicKeyPem"] = key
+                return key
+            case _:
+                return None
